e31583145ff1b7b9e0bd672a5d3f4542b405cfcf
riazhaq <riazhaq03@gmail.com>
2026-02-03 07:56:01 +0800

    Level-0: Rename Duke and add greeting/exit

---
+++ src/main/java/Atlas.java
@@ -0,0 +1,18 @@
+public class Atlas {
+    public static void main(String[] args) {
+        String logo =
+                "  █████╗ ████████╗██╗      █████╗ ███████╗\n"
+                        + " ██╔══██╗╚══██╔══╝██║     ██╔══██╗██╔════╝\n"
+                        + " ███████║   ██║   ██║     ███████║███████╗\n"
+                        + " ██╔══██║   ██║   ██║     ██╔══██║╚════██║\n"
+                        + " ██║  ██║   ██║   ███████╗██║  ██║███████║\n"
+                        + " ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝\n";
+
+
+        System.out.println("Hello! I'm Atlas");
+        System.out.println(logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
