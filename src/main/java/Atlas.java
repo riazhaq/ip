8a839462d8383c77d4822a1ddc160f5a271d74f0
riazhaq <riazhaq03@gmail.com>
2026-02-03 08:01:25 +0800

    Level-1: Echo user input and exit on bye

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Atlas {
     public static void main(String[] args) {
         String logo =
@@ -8,11 +10,23 @@ public class Atlas {
                         + " ██║  ██║   ██║   ███████╗██║  ██║███████║\n"
                         + " ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝\n";
 
-
         System.out.println("Hello! I'm Atlas");
         System.out.println(logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
+
 
